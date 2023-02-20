af07520dc2a6cb180d5723ed4d0ce28829935b42
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-20 02:02:33 -0800

    update base_model & add save and to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@ from datetime import date
 
 class BaseModel:
     
+    
+    
     def __init__(self):
-        id = str(uuid.uuid4())
-        create_at = date.today()
-        update_at = date.today()
+        self.id = str(uuid.uuid4())
+        self.create_at = date.today()
+        self.update_at = date.today()
 
-    def __str__ (self):
-        return f"[{type(self)} ({self.id} {self.__dict__})]"
+    def __str__(self):
+        return f"[{self.__class__.__name__} ({self.id} {self.__dict__})]"
+    
+    def save():
+        
+    
+    def to_dict():
+        
