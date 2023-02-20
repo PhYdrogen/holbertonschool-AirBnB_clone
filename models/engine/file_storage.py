22b44af643f2d856d540d64105216a6283c64c6b
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-20 06:35:31 -0800

    c'était juste ça quoi, modification de la methode new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,15 +18,14 @@ class FileStorage:
         return self.__objects
         
     def new(self, obj):
-        self.__objects = obj
+        self.__objects["{}.{}".format(obj["__class__"], obj["id"])] = obj
         
     def save(self):
-        with open(self.__file_path, "a", encoding="utf-8") as f: 
+        with open(self.__file_path, "w", encoding="utf-8") as f:
             self.__objects = json.dump(self.__objects, f)
 
     def reload(self):
         if not os.path.exists(self.__file_path):
-            
             return
         with open(self.__file_path, "r", encoding="utf-8") as f: 
             self.__objects = json.load(f)
