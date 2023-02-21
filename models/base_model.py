36eefa08acb8a1001b3cee11b8c634d8d3b640c0
Gabriel <Fossecruor@gmail.com>
2023-02-21 16:29:23 +0100

    J'ai refais tout le basemodel marche mieux

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,35 @@
-""" BASE MODEL CLASS """
 import uuid
 from datetime import datetime
 from models import storage
-formatage = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
-    """" Class basemodel """
- 
-    
-    
+    """ Classe base model """
     def __init__(self, *args, **kwargs):
-        """ init """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
                 if key == "created_at":
-                    self.created_at = datetime.strptime(value, formatage)
+                    #str > datetime
+                    self.created_at = datetime.fromisoformat(value)
                 if key == "updated_at":
-                    self.updated_at = datetime.strptime(value, formatage)
-
+                    #str > datetime
+                    self.updated_at = datetime.fromisoformat(value)
         else:
-            self.id = self.getID()
-            self.created_at = self.getDate()
-            self.updated_at = self.getDate()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
-
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__})]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
-        """ fait une sauvegarde de l'instance et actualise updated_at en datetime.datetime """
-        storage.new(self)
         storage.save()
-        self.updated_at = f"{self.getDate()}"
+        self.updated_at = datetime.now()
     
     def to_dict(self):
-        new_dict = self.__dict__
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at
-        new_dict["updated_at"] = self.updated_at
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-    
-    @staticmethod
-    def getDate():
-        """ retourne la date en datetime.datetime """
-        return datetime.now()
-        
-    @staticmethod
-    def getID():
-        return str(uuid.uuid4())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,21 +18,21 @@ class FileStorage:
         
     def new(self, obj):
         obj = obj.to_dict()
-        for k, o in obj.items():
-                if (not isinstance(o, str)):
-                    obj[k] = str(obj[k]) 
-                    
+        #for k, o in obj.items():
+        #        if (not isinstance(o, str)):
+        #            obj[k] = str(obj[k])    
         self.__class__.__objects["{}.{}".format(obj["__class__"], obj["id"])] = obj
         
     def save(self):
-        
         with open(self.__class__.__file_path, "w", encoding="utf-8") as f:
+            #print(self.__class__.__objects)
+            #for l,k in self.__class__.__objects.items():
+            #    print("{} {}".format(k.get(id, "PAS D ID"), l))
+
             json.dump(self.__class__.__objects, f)
-        f.close()
 
     def reload(self):
         if not os.path.exists(self.__class__.__file_path):
             return
         with open(self.__class__.__file_path, "r", encoding="utf-8") as f:
             self.__class__.__objects = json.load(f)
-        f.close()
