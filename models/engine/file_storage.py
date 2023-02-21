ab28c6137f2262d9eb96948e79c61061b50b7b50
Gabriel <Fossecruor@gmail.com>
2023-02-21 08:33:41 +0100

    Filestorage ; déplacement des attributs en CLASSE

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,22 +10,24 @@ class FileStorage:
     save
     reload
     """
-    def __init__(self, file = "", obj = {}):
-        self.__file_path = file
-        self.__objects = obj
-        
+    __file_path = "file.json"
+    __objects = {}
+
     def all(self):
-        return self.__objects
+        return self.__class__.__objects
         
     def new(self, obj):
-        self.__objects["{}.{}".format(obj["__class__"], obj["id"])] = obj
+        obj = obj.to_dict()
+        self.__class__.__objects["{}.{}".format(obj["__class__"], obj["id"])] = obj
         
     def save(self):
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            self.__objects = json.dump(self.__objects, f)
+        with open(self.__class__.__file_path, "w", encoding="utf-8") as f:
+            json.dump(self.__class__.__objects, f)
+        f.close()
 
     def reload(self):
-        if not os.path.exists(self.__file_path):
+        if not os.path.exists(self.__class__.__file_path):
             return
-        with open(self.__file_path, "r", encoding="utf-8") as f: 
-            self.__objects = json.load(f)
+        with open(self.__class__.__file_path, "r", encoding="utf-8") as f: 
+            self.__class__.__objects = json.load(f)
+        f.close()
