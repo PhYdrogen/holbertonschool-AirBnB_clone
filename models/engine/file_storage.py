c4aa76ebd393326f3e41859b589f848b269d703d
PhYdrogen <Fossecruor@gmail.com>
2023-02-22 14:07:12 +0100

    Remove the conflix, it's weird

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,6 @@ class FileStorage:
             json.dump(obj_to_dict, f)
 
     def reload(self):
-<<<<<<< HEAD
         if not os.path.exists(self.__class__.__file_path):
             with open(self.__class__.__file_path, "w", encoding="utf-8") as f:
                 json.dump({}, f)
@@ -36,14 +35,3 @@ class FileStorage:
         with open(self.__class__.__file_path, "r", encoding="utf-8") as f:
             self.__class__.__objects = json.load(f)
         f.close()
-=======
-        from models.base_model import BaseModel
-        if not os.path.exists(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            big_dict = json.load(f)
-            for baseid, value in big_dict.items():
-                classe = value["__class__"]
-                obj = eval(classe)(**value)
-                self.new(obj)
->>>>>>> 04dc0b46d854a77f2f372f8b40d815321934b8bf
