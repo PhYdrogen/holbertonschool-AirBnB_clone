fbfee3312baa87a14278bbd740834e0a01694970
Gabriel <Fossecruor@gmail.com>
2023-02-22 17:35:57 +0100

    modify back the file_storage and add update to console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,10 +28,13 @@ class FileStorage:
             json.dump(obj_to_dict, f)
 
     def reload(self):
+        from models.base_model import BaseModel
         if not os.path.exists(self.__class__.__file_path):
-            with open(self.__class__.__file_path, "w", encoding="utf-8") as f:
-                json.dump({}, f)
+            return
             
-        with open(self.__class__.__file_path, "r", encoding="utf-8") as f:
-            self.__class__.__objects = json.load(f)
-        f.close()
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            big_dict = json.load(f)
+            for baseid, value in big_dict.items():
+                classe = value["__class__"]
+                obj = eval(classe)(**value)
+                self.new(obj)
