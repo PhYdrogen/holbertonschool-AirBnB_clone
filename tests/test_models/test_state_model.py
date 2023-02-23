60a33b823408e0d49ea733d13a471dbda4dfd458
RedginaldGodeau <5657@holbertonstudents.com>
2023-02-23 11:59:10 -0800

    add all unitests

---
+++ tests/test_models/test_state_model.py
@@ -0,0 +1,12 @@
+import unittest
+from models.state import State
+
+class Teststate(unittest.TestCase):
+    
+    def test_name(self):
+        State.name = "Toulouse"
+        self.assertEqual(State.name, "Toulouse")
+
+if __name__ == '__main__':
+    unittest.main()
+ 
