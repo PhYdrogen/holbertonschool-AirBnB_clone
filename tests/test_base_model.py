113df97b6110c370ede97d28b41e18ab25a9fbf2
Gabriel <fossecruor@gmail.com>
2023-02-24 00:30:32 -0800

    copie des tests dans tests/

---
+++ tests/test_base_model.py
@@ -0,0 +1,36 @@
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class Testbase(unittest.TestCase):
+    
+    def test_id(self):
+        md = BaseModel()
+        self.assertEqual(type(md.id), str)
+    
+    def test_updateat(self):
+        md = BaseModel()
+        last_type = type(md.updated_at)
+        d_json = md.to_dict()
+        new_type = type(d_json['updated_at'])
+        self.assertFalse(last_type == new_type)
+        
+    def test_updateat(self):
+        md = BaseModel()
+        self.assertTrue(isinstance(md.created_at, datetime))
+        
+    def test_save(self):
+        bm = BaseModel()
+        bmbeforesave = bm.to_dict()
+        bm.save()
+        self.assertFalse(bm.to_dict() != bmbeforesave)
+        
+    def test_str(self):
+        md = BaseModel()
+        self.assertEqual(type(str(md)), str)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+ 
