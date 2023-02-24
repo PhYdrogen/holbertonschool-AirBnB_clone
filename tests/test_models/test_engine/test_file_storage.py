e617a29e1cee666cc7c83f840e69a5a05445443a
Gabriel <Fossecruor@gmail.com>
2023-02-24 15:18:33 +0100

    change test save with test_save from base_model

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -31,14 +31,25 @@ class Teststorage(unittest.TestCase):
 
     def test_save(self):
         from models.base_model import BaseModel
+        import datetime
         fs = FileStorage()
-        item = BaseModel()
-        fs.new(item)
-        fs.save()
-
-        save_storage = fs.all()
         fs.reload()
-        self.assertTrue(save_storage == fs.all())
+        if os.path.exists("file.json"): # si il existe sort une erreur
+            self.assertTrue(False)
+
+        bm = BaseModel()
+        before = bm.updated_at
+        bm.save()
+        if not os.path.exists("file.json"): # si il existe PAS sort une erreur
+            self.assertTrue(False)
+        self.assertTrue(before == bm.updated_at)
+        self.assertEqual(type(before), datetime)
+        self.assertEqual(type(bm.updated_at), datetime)
+        fs.reload()
+        all_obj_as_dict = fs.all()
+        for key, obj in all_obj_as_dict.items():
+            self.assertEqual(key, "{}.{}".format(obj.__class__.__name__, obj.id))
+            self.assertTrue(isinstance(obj, BaseModel))
 
     def test_reload(self):
         fs = FileStorage()
